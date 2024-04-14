7f9c6dc33fce6701646293e091a04f0dbaae7097
Debiddo <dzheng@terpmail.umd.edu>
2024-04-14 16:55:54 -0400

    corrected the calculation of width for each square

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 const containerEL = document.querySelector(".container");
+const buttonEl = document.getElementById("change-square");
+
+buttonEl.addEventListener("click",() => {
+    let numberOfSquares = prompt("Enter the number of squares (10-100): ");
+    while(numberOfSquares > 100 || numberOfSquares < 10 ){
+        numberOfSquares = prompt("Enter a valid number of squares (10-100): ");
+    }
+    containerEL.innerHTML = "";
+    let width = (100/numberOfSquares);
+
+    for(let i=0;i<numberOfSquares;i++){
+        for(let j=0;j<numberOfSquares;j++){
+            let squareEl = document.createElement('div');
+            squareEl.classList.add("square");
+            squareEl.style.flexBasis = `${width}%`;
+            squareEl.addEventListener("mouseover",handleMouseOver);
+            containerEL.appendChild(squareEl);
+        }
+    }
+});
 
 const handleMouseOver = (event) => {
     console.log
@@ -9,6 +29,7 @@ for(let i=0;i<16;i++){
     for(let j=0;j<16;j++){
         let squareEl = document.createElement('div');
         squareEl.classList.add("square");
+        squareEl.style.flexBasis = `${100/16}%`;
         squareEl.addEventListener("mouseover",handleMouseOver);
         containerEL.appendChild(squareEl);
     }
